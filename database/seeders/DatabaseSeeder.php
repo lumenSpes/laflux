e2b26c52dc9944782330e472863d0b36d6edf294
lumenSpes <lumenseps.zee@gmail.com>
2024-07-12 21:12:24 +0600

    inital commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $this->call(UsersSeeder::class);
+         $this->call(GroupsSeeder::class);
+         $this->call(ExtensionManagerSeeder::class);
+         $this->call(ACLSeeder::class);
+    }
+}
